15edfc9e7b12fed2092fa54cb6ed86819e0aa9ae
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-30 20:15:38 -0500

    Proyecto

---
+++ src/main/java/zamira/estudiante/automovil1/Automovil1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package zamira.estudiante.automovil1;
+
+/**
+ *
+ * @author user
+ */
+public class Automovil1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
